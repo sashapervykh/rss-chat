d18c2bf81306bc7466cae046f815f6429b4e18c8
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-04-14 11:26:47 +0300

    feat: add eslint to the project

---
+++ fun-chat/eslint.config.mjs
@@ -0,0 +1,23 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  {
+    linterOptions: {
+      noInlineConfig: true,
+      reportUnusedDisableDirectives: "error",
+    },
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "no-unused-expressions": "error",
+    },
+  }
+);
