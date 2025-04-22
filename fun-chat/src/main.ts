86a4e97836ceec01786cc31af836ea1a8031a7ab
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-04-22 10:10:32 +0300

    refactor: delete empty files and unnecessary comments

--- fun-chat/src/main.ts
+++ fun-chat/src/main.ts
@@ -1,8 +1,3 @@
-// import LoginPage from './app/page/login-page/login-page';
 import startRouting from './app/router/router';
 
-// const authorizationPage = new LoginPage();
-// authorizationPage.createLoginPage();
-
-// history.replaceState('login', '', '/login');
 startRouting();
